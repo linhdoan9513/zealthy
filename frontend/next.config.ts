f3e42cc93bf4e848a7d158fe594b711981078b0a
linhdoan9513 <doanlinh9513@gmail.com>
2025-08-02 15:05:05 -0400

    more fix for deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
